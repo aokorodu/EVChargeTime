59416e5770fa5c8f0aa5a80dc9a6c250091283fc
aokorodu <aokorodu@gmail.com>
2023-10-23 22:13:37 -0400

    intro rewrite

--- src/components/Intro/index.tsx
+++ src/components/Intro/index.tsx
@@ -16,16 +16,21 @@ const Intro = ({ callback }: IntroProps) => {
                         <div className={styles.mainTitle}>80 or 100?</div>
                     </header>
                     <section>
-                        <h1>Question: In the long run, what saves the most time: charging to 80%, or 100%?*</h1>
+                        <h1>Over the long run, what saves the most time? Charging less often, but to 100%, or charging more often, but only to 80%? *</h1>
                         <div>
-                            <strong>Disclaimer:</strong>
-                            <div>This is not meant to be a scientifically accurate study on charging!  I don’t have access to the official charging curves for EGMP cars, and the results are based on charging curve estimates cobbled together from several different sources. For entertainment purposes only!</div>
+                            <strong>GOAL:</strong>
+                            <p>To help you determine the <strong>best charging strategy</strong> based upon how long it takes you (on average) to drive to a charger, how long you have to wait (on average) to charge, and the average full-charge range of your vehicle.</p>
+                        </div>
+                        <div>
+                            <strong>DISCLAIMER:</strong>
+                            <p>This is not meant to be a scientifically accurate study!  I don’t have access to official charging curves for EGMP cars, and the results are based entirely on estimates cobbled together from several different sources. <strong>For entertainment purposes only!</strong></p>
+                            <p>In addition, this app doesn't consider the long-term effect of various charging strategies on battery health. Nor will it look at the ethical/moral question of charging to 100% when people are waiting. It only looks at one thing: <strong>TIME</strong></p>
                         </div>
                     </section>
 
                     <section>
-                        <strong>Assumptions:</strong>
-                        <div>This app makes the following highly unrealistic assumptions:</div>
+                        <strong>ASSUMPTIONS:</strong>
+                        <p>This app makes the following highly unrealistic assumptions:</p>
                         <ol>
                             <li>The cars are always charged at 350 kW DC chargers at the maximum possible speed</li>
                             <li>Charging occurs at the same speed, every time.</li>
